f769053139b414c3d2938cdc047e310bd68ce62d
urmish <s.urmish@gmail.com>
2019-01-15 18:50:44 -0500

    restructing

--- hackerrank_practice/flipping_bits.cpp
+++ hackerrank_practice/flipping_bits.cpp
@@ -7,18 +7,7 @@
 #include <limits>
 #include <vector>
 
-std::string primality(uint64_t n) {
-  auto sq_root = static_cast<uint64_t>(sqrt(n));
-  if (n <= 1) {
-    return {"Not prime", std::allocator<char>()};
-  }
-  for (uint64_t i = 2; i <= sq_root; i++) {
-    if (n % i == 0) {
-      return {"Not prime", std::allocator<char>()};
-    }
-  }
-  return {"Prime", std::allocator<char>()};
-}
+uint32_t flippingBits(uint32_t n) { return (static_cast<uint32_t>(-1) - n); }
 
 int main(int argc, const char **argv) {
   std::ifstream fin(getenv("INPUT_PATH"), std::ios_base::in);
@@ -27,13 +16,16 @@ int main(int argc, const char **argv) {
   /* std::string str; */
   uint32_t q;
   fin >> q;
+  fin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
   /* std::cout << "q is: " << q << std::endl; */
 
   for (uint32_t q_itr = 0; q_itr < q; q_itr++) {
     uint32_t n;
     fin >> n;
+    fin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
     /* std::cout << "n is: " << n << std::endl; */
-    std::string result = primality(n);
+    uint32_t result = flippingBits(n);
     /* std::cout << "result is: " << result << std::endl; */
     fout << result << "\n";
   }
--- hackerrank_practice/primality.cpp
+++ hackerrank_practice/primality.cpp
@@ -7,8 +7,17 @@
 #include <limits>
 #include <vector>
 
-uint32_t flippingBits (uint32_t n) {
-  return (static_cast<uint32_t>(-1) - n);
+std::string primality(uint64_t n) {
+  auto sq_root = static_cast<uint64_t>(sqrt(n));
+  if (n <= 1) {
+    return {"Not prime", std::allocator<char>()};
+  }
+  for (uint64_t i = 2; i <= sq_root; i++) {
+    if (n % i == 0) {
+      return {"Not prime", std::allocator<char>()};
+    }
+  }
+  return {"Prime", std::allocator<char>()};
 }
 
 int main(int argc, const char **argv) {
@@ -18,13 +27,15 @@ int main(int argc, const char **argv) {
   /* std::string str; */
   uint32_t q;
   fin >> q;
+  fin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
   /* std::cout << "q is: " << q << std::endl; */
 
   for (uint32_t q_itr = 0; q_itr < q; q_itr++) {
     uint32_t n;
     fin >> n;
+    fin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     /* std::cout << "n is: " << n << std::endl; */
-    uint32_t result = flippingBits(n);
+    std::string result = primality(n);
     /* std::cout << "result is: " << result << std::endl; */
     fout << result << "\n";
   }
