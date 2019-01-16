90054bb5fe2a2b6e5a1f9c9742a64fb2b3f7a66b
urmish <s.urmish@gmail.com>
2019-01-16 16:21:45 -0500

    Friend circle queries, implemented using list and maps

---
+++ hackerrank_practice/friend_circle_queries.cpp
@@ -0,0 +1,159 @@
+/* Primality: https://www.hackerrank.com/challenges/ctci-big-o/problem */
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <vector>
+
+std::shared_ptr<std::list<uint32_t>> createList() {
+  return std::make_shared<std::list<uint32_t>>();
+}
+
+std::vector<uint32_t>
+maxCircle(const std::vector<std::vector<uint32_t>> &queries) {
+  std::map<uint32_t, std::shared_ptr<std::list<uint32_t>>> int_map;
+  std::vector<uint32_t> result;
+  uint32_t max = 0;
+  for (auto &elem : queries) {
+    /* std::cout << elem[0]<< ":" << elem[1] << std::endl; */
+    if (int_map[elem[0]] == nullptr && int_map[elem[1]] == nullptr) {
+      /* std::cout << "A: elem[0]:elem[1] is: " << elem[0] << ":" << elem[1] <<
+       * std::endl; */
+      auto new_list = createList();
+      /* new_list->resize(2); */
+      new_list->push_back(elem[0]);
+      int_map[elem[0]] = new_list;
+      new_list->push_back(elem[1]);
+      int_map[elem[1]] = new_list;
+      /* std::cout<< "A: new_list size is: " << new_list->size() << std::endl;
+       */
+      if (max < new_list->size()) {
+        max = new_list->size();
+      }
+    } else if (int_map[elem[0]] != nullptr && int_map[elem[1]] == nullptr) {
+      auto list_ptr = int_map[elem[0]];
+      /* uint32_t list_size = list_ptr->size(); */
+      /* std::cout << "B: elem[0]:elem[1] is: " << elem[0] << ":" << elem[1] <<
+       * std::endl; */
+      /* list_ptr->resize(list_size + 1); */
+      list_ptr->push_back(elem[1]);
+      int_map[elem[1]] = list_ptr;
+      if (max < list_ptr->size()) {
+        max = list_ptr->size();
+      }
+    } else if (int_map[elem[0]] == nullptr && int_map[elem[1]] != nullptr) {
+      auto list_ptr = int_map[elem[1]];
+      /* uint32_t list_size = list_ptr->size(); */
+      /* std::cout << "C: elem[0]:elem[1] is: " << elem[0] << ":" << elem[1] <<
+       * std::endl; */
+      /* list_ptr->resize(list_size + 1); */
+      list_ptr->push_back(elem[0]);
+      int_map[elem[0]] = list_ptr;
+      if (max < list_ptr->size()) {
+        max = list_ptr->size();
+      }
+    } else {
+      /* std::cout << "D: elem[0]:elem[1] is: " << elem[0] << ":" << elem[1] <<
+       * std::endl; */
+      if (int_map[elem[0]] != int_map[elem[1]]) {
+        if (int_map[elem[0]]->size() > int_map[elem[1]]->size()) {
+          auto list_ptr = int_map[elem[0]];
+          auto temp_list_ptr = int_map[elem[1]];
+          for (auto i = temp_list_ptr->begin(); i != temp_list_ptr->end();) {
+            /* std::cout << "E: elem_new is: " << *i << std::endl; */
+            /* auto list_size = list_ptr->size(); */
+            /* list_ptr->resize(list_size+1); */
+            list_ptr->push_back(*i);
+            int_map[*i] = list_ptr;
+            i = temp_list_ptr->erase(i);
+          }
+          /* std::cout << "E: end" << std::endl; */
+          /* std::cout << "E: int_map[elem[0]]->size() is: " << list_ptr->size()
+           * << std::endl; */
+          /* std::cout << "E: and the list int_map[elem[0]] is: "; */
+          /* for (auto element : *int_map[elem[0]]) */
+          /*   std::cout << element << ":" ; */
+          /* std::cout << std::endl; */
+          /* std::cout << "E: max is: " << max << std::endl; */
+          /* std::cout << "E: int_map[elem[0]]->size() is: " << list_ptr->size()
+           * << std::endl; */
+          if (max < list_ptr->size()) {
+            max = list_ptr->size();
+          }
+        } else {
+          auto list_ptr = int_map[elem[1]];
+          auto temp_list_ptr = int_map[elem[0]];
+          for (auto i = temp_list_ptr->begin(); i != temp_list_ptr->end();) {
+            /* std::cout << "F: elem_new is: " << *i << std::endl; */
+            /* auto list_size = list_ptr->size(); */
+            /* list_ptr->resize(list_size+1); */
+            list_ptr->push_back(*i);
+            int_map[*i] = list_ptr;
+            i = temp_list_ptr->erase(i);
+          }
+          /* std::cout << "F: end" << std::endl; */
+          /* std::cout << "F: int_map[elem[1]]->size() is: " << list_ptr->size()
+           * << std::endl; */
+          /* std::cout << "F: and the list int_map[elem[1]] is: "; */
+          /* for (auto element : *int_map[elem[1]]) */
+          /*   std::cout << element << ":" ; */
+          /* std::cout << "F: max is: " << max << std::endl; */
+          /* std::cout << "F: int_map[elem[1]]->size() is: " << list_ptr->size()
+           * << std::endl; */
+          if (max < list_ptr->size()) {
+            max = list_ptr->size();
+          }
+        }
+      } else {
+        if (max < int_map[elem[0]]->size()) {
+          max = int_map[elem[0]]->size();
+        }
+      }
+    }
+    /* std::cout << "current max is: " << max << std::endl; */
+    result.push_back(max);
+  }
+  return result;
+}
+
+int main(int argc, const char **argv) {
+  std::ifstream fin(getenv("INPUT_PATH"), std::ios_base::in);
+  std::ofstream fout(getenv("OUTPUT_PATH"), std::ios_base::out);
+
+  /* std::string str; */
+  uint32_t q;
+  fin >> q;
+  fin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+  /* std::cout << "q is: " << q << std::endl; */
+
+  std::vector<std::vector<uint32_t>> queries(q,std::allocator<std::vector<std::vector<uint32_t>>>());
+  for (uint32_t i = 0; i < q; i++) {
+    queries[i].resize(2);
+    for (uint32_t j = 0; j < 2; j++) {
+      fin >> queries[i][j];
+    }
+    fin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+  }
+
+  std::vector<uint32_t> ans = maxCircle(queries);
+
+  for (uint32_t i = 0; i < ans.size(); ++i) {
+    fout << ans[i];
+    if (i != ans.size() - 1) {
+      fout << "\n";
+    }
+  }
+
+  fout << "\n";
+  fout.close();
+
+  (void)argc;
+  (void)argv;
+  return 0;
+}
