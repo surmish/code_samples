82acd2aa34e96b319d842f2a104b9ea46dda6896
urmish <s.urmish@gmail.com>
2021-05-15 22:06:32 -0700

    cleanup

--- miscell/conways_game_of_life.py
+++ miscell/conways_game_of_life.py
@@ -1,9 +1,7 @@
 import sys
 import argparse
 import random
-import time
 import curses
-from curses import wrapper
 
 def calcRowsAndColumns(grid):
     rows = len(grid)
@@ -14,9 +12,7 @@ def calcRowsAndColumns(grid):
     return rows, columns
 
 
-def printGrid (grid, stdoutOrStdScr = False, stdscr = 0, y_coordinate = 0, x_coordinate = 0):
-    rows, columns = calcRowsAndColumns(grid)
-
+def printGrid (grid, stdoutOrStdScr = False, stdscr = 0, x_coordinate = 0, y_coordinate = 0):
     for row in range(len(grid)):
         row_str = ""
         for column in range(len(grid[row])):
@@ -39,12 +35,8 @@ def getAliveNeighborCount(i,j):
     return count
 
 def cellNextState(i, j):
-    rows, columns = calcRowsAndColumns(grid)
-
-    currentState = grid[i][j]
     nextState = ' '
     count = getAliveNeighborCount(i,j)
-
     if grid[i][j] == 'X':
         if 2 <= count <= 3:
             nextState = 'X'
@@ -55,53 +47,40 @@ def cellNextState(i, j):
 
 def createNextStateGrid():
     rows, columns = calcRowsAndColumns(grid)
-    nextStateGrid = [ [ ' ' for c in range(columns) ] for r in range(rows)]
-
-    for r in range(rows):
-        for c in range(columns):
-            nextStateGrid[r][c] = cellNextState(r,c)
-
+    nextStateGrid = [ [ cellNextState(r,c) for c in range(columns) ] for r in range(rows)]
     return nextStateGrid
 
 def draw_menu(stdscr):
     global grid
     rows, columns = calcRowsAndColumns(grid)
-
     k = 0
-
     height, width = stdscr.getmaxyx()
-    print("max number of rows:columns allowed are: " + str(rows) + ":" + str(columns))
-    if rows > height - 2 or columns > width - 1:
-        print("max number of rows:columns allowed are: " + str(rows) + ":" + str(columns))
+    if rows > (height - 1) or columns > width:
+        stdscr.clear()
+        stdscr.refresh()
+        sys.stdout.write("max number of rows:columns allowed are: " + str(height - 1) + ":" + str(width))
+        sys.stdout.write("")
         return
-
     # Clear and refresh the screen for a blank canvas
     stdscr.clear()
     stdscr.refresh()
-
     # Start colors in curses
     curses.start_color()
     curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_WHITE)
-
     # Loop where k is the last character pressed
     while (k != ord('q')):
-
         # Initialization
         stdscr.clear()
         height, width = stdscr.getmaxyx()
-
         x_coordinate = int((width // 2) - (columns // 2) - (columns % 2))
         y_coordinate = int((height // 2) - (rows // 2) - (rows % 2))
-
         stdscr.attron(curses.color_pair(1))
-        printGrid(grid, True, stdscr, y_coordinate, x_coordinate)
+        printGrid(grid, True, stdscr, x_coordinate, y_coordinate)
         stdscr.attron(curses.color_pair(1))
         grid = createNextStateGrid()
-
         # Declaration of strings
         statusbarstr = "Press 'q' to exit | Press any other key to progress the game"
-
         # Render status bar
         stdscr.attron(curses.color_pair(2))
         if len(statusbarstr) >= width:
@@ -109,44 +88,31 @@ def draw_menu(stdscr):
         stdscr.addstr(height-1, 0, statusbarstr)
         stdscr.addstr(height-1, len(statusbarstr), " " * (width - len(statusbarstr) - 1))
         stdscr.attroff(curses.color_pair(2))
-
         # Refresh the screen
         stdscr.refresh()
-
         # Wait for next input
         k = stdscr.getch()
 
 def main():
-
     global grid
-
     parser = argparse.ArgumentParser(description='Grid dimensions in the conways game of life.')
     parser.add_argument("-r","--rows", help="number of rows in the grid", type=int, default = 20)
     parser.add_argument("-c","--columns", help="number of columns in the grid", type=int, default = 60)
     args = parser.parse_args()
-
     rows = args.rows
     columns = args.columns
-
-    grid = [ [ ' ' for c in range(columns) ] for r in range(rows)]
-
+    grid = [ [ ' ' for _ in range(columns) ] for _ in range(rows)]
     num_of_cells = rows * columns
-
     cells_alive_initially = random.randint(0, num_of_cells // 3)
-
     if cells_alive_initially == 0 :
         cells_alive_initially = 1 
-
     # print(str(cells_alive_initially) + " out of " + str(num_of_cells) + " cells alive initially")
-
-    for i in range(cells_alive_initially):
+    for _ in range(cells_alive_initially):
         cell_num = random.randint(0, num_of_cells - 1)
         r = cell_num // columns
         c = cell_num % columns
         grid[r][c] = 'X'
-
     curses.wrapper(draw_menu)
-
     # printGrid(grid)
 
 if __name__ == "__main__":
