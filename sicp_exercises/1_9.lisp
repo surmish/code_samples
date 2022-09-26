05b84f542232989cd028f3f04149dd17ca0494d7
urmish <s.urmish@gmail.com>
2022-09-25 22:15:15 -0700

    name fix

--- sicp_exercises/1_9.lisp
+++ sicp_exercises/1_9.lisp
@@ -11,8 +11,8 @@
 ; not just in the variables, a and b, but also in the 'recursion'
 ; stack
 
-(define (+-2 a b)
-  (if (= a 0) b (+-2 (dec a) (inc b))))
+(define (+_alt a b)
+  (if (= a 0) b (+_alt (dec a) (inc b))))
 ; this is a tail-recursive implementation (iterative process), since
 ; the state of the process is stored in a and b at any given time and
 ; can be used to compute the result of the process.
