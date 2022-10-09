bbd7210b9518c8948502563a5c846e663623be1e
urmish <s.urmish@gmail.com>
2022-10-08 17:04:21 -0700

    1.31 linear recursion and iterative produc procedure

--- sicp_exercises/1_31.rkt
+++ sicp_exercises/1_31.rkt
@@ -1,16 +1,31 @@
 #!/usr/bin/racket
 #lang racket/base
 
+; recursive
 (define (product term a next b)
   (if (> a b) 1
       (* (term a) (product term (next a) next b))
       ))
 
-(define (factorial n) (product identity 1 inc n))
+; linear iterative
+(define (product-iter term a next b)
+  (define (iter a result) 
+    (if (> a b)
+        result
+        (iter (next a) (* (term a) result))
+      )
+    )
+  (iter a 1)
+  )
+
+(define (factorial n) (product-iter identity 1 inc n))
 (define (pi-term a) (/ (* (* a 2.0) (* (+ a 1) 2)) (square (+ (* 2 a) 1))))
 (define (square x) (* x x))
 (define (inc a) (+ a 1))
 (define (identity a) a)
 
-(factorial 5)
-(* 4 (product pi-term 1 inc 10000))
+(product      identity 1 inc 5)
+(product-iter identity 1 inc 5)
+
+(* 4 (product      pi-term 1 inc 10000))
+(* 4 (product-iter pi-term 1 inc 10000))
